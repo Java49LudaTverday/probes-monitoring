a5c423a18416c3fdda198e153e0c4879823e26c4
LudaTverday <Ludachka22@gmail.com>
2024-01-30 18:13:21 +0200

    adding logging for Controller

--- email-data-provider/src/main/java/telran/email_provider/controller/EmailDataProviderController.java
+++ email-data-provider/src/main/java/telran/email_provider/controller/EmailDataProviderController.java
@@ -6,16 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import telran.email_provider.service.EmailDataProviderService;
 import telran.probes.dto.EmailData;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class EmailDataProviderController {
 final EmailDataProviderService emailProviderService;
 
-@GetMapping
-List<EmailData> getEmailsPersons (long sensorId){
+@GetMapping("/{id}")
+ List<EmailData> getEmailsPersons (long sensorId){
+	log.debug("received sensor id {}", sensorId);
 	return emailProviderService.getEmailsResponsiblePersons(sensorId);
 }
 }
