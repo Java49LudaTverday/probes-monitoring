e20a645cb5d39db33c176eaaa3459efcc30a506c
LudaTverday <Ludachka22@gmail.com>
2024-02-10 16:49:09 +0200

    added property for embedded mongoDb, changed sensorDataPopulation component

--- sensors/src/main/java/telran/probes/SensorsDataPopulation.java
+++ sensors/src/main/java/telran/probes/SensorsDataPopulation.java
@@ -2,12 +2,15 @@ package telran.probes;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import telran.probes.configuration.SensorData;
+import telran.probes.configuration.SensorsConfiguration;
 import telran.probes.model.SensorEmailDoc;
 import telran.probes.model.SensorRangeDoc;
 import telran.probes.repo.SensorEmailRepo;
@@ -15,23 +18,42 @@ import telran.probes.repo.SensorRangeRepo;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SensorsDataPopulation {
- 
-	final static List<SensorRangeDoc> sensorsRanges = List.of( new SensorRangeDoc(123l, 100f, 200f),
-			new SensorRangeDoc(124l, -10f, 20f),
-			new SensorRangeDoc(125l, -10, 20)) ;
-	final static List<SensorEmailDoc> sensorsEmails = List.of( new SensorEmailDoc(123l, new String[] { "ludachka22@gmail.com" }),
-			new SensorEmailDoc(124l, new String[] { "ludachka22@gmail.com" }),
-			new SensorEmailDoc(125l, new String[] { "ludachka22@gmail.com" })) ;
-	
-	final SensorEmailRepo sensorsEmailRepo;
-	final SensorRangeRepo sensorsRangesRepo;
-	
-	@PostConstruct
-	void initialPopulation () {
-		sensorsEmailRepo.deleteAll();
-		sensorsRangesRepo.deleteAll();
-		sensorsEmailRepo.saveAll(sensorsEmails);
-		sensorsRangesRepo.saveAll(sensorsRanges);
+final SensorEmailRepo sensorEmailsRepo;
+final SensorRangeRepo sensorRangeRepo;
+final SensorsConfiguration sensorsConfiguration;
+@PostConstruct
+void dbPopulation() {
+	Map<Long, SensorData> sensorDataMap = sensorsConfiguration.getSensorsDataMap();
+	List<SensorEmailDoc> sensorsEmails = sensorEmailsRepo.findAll();
+	List<SensorRangeDoc> sensorsRanges = sensorRangeRepo.findAll();
+	if(sensorsEmails.isEmpty()) {
+		populateEmails(sensorDataMap);
+	} else {
+		log.debug("Collection  sensors-emails already contains {} documents", sensorsEmails.size());
 	}
+	if (sensorsRanges.isEmpty()) {
+		populateRanges(sensorDataMap);
+	} else {
+		log.debug("Collection  sensors-ranges already contains {} documents", sensorsRanges.size());
+	}
+	
+}
+private void populateRanges(Map<Long, SensorData> sensorDataMap) {
+	List<SensorRangeDoc> documents = sensorDataMap.entrySet().stream()
+			.map(e -> new SensorRangeDoc(e.getKey(), e.getValue().minValue(),
+					e.getValue().maxValue())).toList();
+	sensorRangeRepo.saveAll(documents);
+	log.debug("{} has been saved", documents.size());
+	
+	
+}
+private void populateEmails(Map<Long, SensorData> sensorDataMap) {
+	List<SensorEmailDoc> documents = sensorDataMap.entrySet().stream()
+			.map(e -> new SensorEmailDoc(e.getKey(), e.getValue().emails())).toList();
+	sensorEmailsRepo.saveAll(documents);
+	log.debug("{} has been saved", documents.size());
+	
+}
 }
--- sensors/src/test/java/telran/probes/SensorsServiceTest.java
+++ sensors/src/test/java/telran/probes/SensorsServiceTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.stream.binder.test.*;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.messaging.Message;
 
@@ -15,30 +17,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import telran.probes.configuration.SensorsConfiguration;
 import telran.probes.dto.ProbeData;
+
 @SpringBootTest
 @Slf4j
 @Import(TestChannelBinderConfiguration.class)
-class SensorsTest {
+class SensorsServiceTest {
 	@Autowired
-OutputDestination consumer;
+	OutputDestination consumer;
 	@Autowired
 	SensorsConfiguration sensorsConfiguration;
-	
 	@Test
-	void test() throws Exception{
+	void test() throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		String bindingName = sensorsConfiguration.getBindingName();
 		long timestamp = System.currentTimeMillis();
 		while (System.currentTimeMillis() - timestamp < SensorsAppl.TIMEOUT) {
 			Message<byte[]> message = consumer.receive(1000, bindingName);
-			if(message != null) {
-				ProbeData probeData = mapper.readValue( message.getPayload(),
-						ProbeData.class);
+			if (message != null) {
+				ProbeData probeData = mapper.readValue(message.getPayload(), ProbeData.class);
 				log.debug("test: {}", probeData);
 			}
 			Thread.sleep(1000);
-			
-			
+
 		}
 	}
 
