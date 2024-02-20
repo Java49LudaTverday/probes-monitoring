5a9d77307b63a988bab55f2421fa1a742fc48bbf
LudaTverday <Ludachka22@gmail.com>
2024-02-20 18:30:24 +0200

    added security with roles

--- gateway/src/main/java/telran/probes/configuration/SecurityConfiguration.java
+++ gateway/src/main/java/telran/probes/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package telran.probes.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,7 +24,13 @@ public class SecurityConfiguration {
 	SecurityFilterChain configure (HttpSecurity http) throws Exception {
 		http.cors(custom -> custom.disable());
 		http.csrf(custom -> custom.disable());
-		http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+		http.authorizeHttpRequests(requests -> requests
+				.requestMatchers("/sensors/emails").hasRole("EMAILS_ADMIN")
+				.requestMatchers("/rensors/range").hasRole("RANGES_ADMIN")
+				.requestMatchers("/accounts").hasRole("ACCOUNTS_USER")
+				.requestMatchers("/emails/sensor").hasRole("EMAILS_USER")
+				.requestMatchers("/range/sensor").hasRole("RANGES_USER")
+				.anyRequest().authenticated());
 		http.httpBasic(Customizer.withDefaults());
 		return http.build();
 	}
